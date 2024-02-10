4bd1ce714daccf6c96cbf9cb0d7b7a3cf141783c
Lior Cohen <lior_cohen@bonify.de>
2024-02-10 17:08:57 +0100

    added async storage

--- App.tsx
+++ App.tsx
@@ -1,12 +1,66 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, FlatList } from 'react-native';
 import theme from './constants/theme';
+import { horizontalScale, verticalScale } from './helpers/scaleHelpers';
+import { getDataFromAsyncStorage } from './helpers/asyncStorageHelpers';
+
+export interface Item {
+  content: string;
+  checked: boolean;
+}
+export interface ItemWithId extends Item {
+  id: string | null;
+}
 
 export default function App() {
+  const [shoppingList, setShoppingList] = React.useState<ItemWithId[]>([]);
+
+  React.useEffect(() => {
+    const getShoppingList = async () => {
+      const getDataResponse = await getDataFromAsyncStorage({
+        key: 'shoppingList',
+      });
+      if (getDataResponse.status === 'SUCCESS') {
+        if (getDataResponse.data) {
+          setShoppingList(getDataResponse.data);
+        }
+      }
+    };
+    getShoppingList();
+  }, []);
+
+  // clear async storage
+  // React.useEffect(() => {
+  //   clearAsyncStorage();
+  // }, []);
+
+  // write to async storage
+  // React.useEffect(() => {
+  //   storeDataInAsyncStorage({
+  //     key: 'shoppingList',
+  //     value: [{ id: '1', content: 'Milks', checked: true }],
+  //   });
+  // }, []);
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.header}>Better Shopping List</Text>
-      <StatusBar style="auto" />
+      <View style={styles.innerContainer}>
+        <StatusBar style="auto" />
+        <Text style={styles.header}>Better Shopping List</Text>
+        <FlatList
+          contentContainerStyle={styles.list}
+          data={shoppingList}
+          renderItem={({ item }) => (
+            <View style={styles.listItem} key={item.id}>
+              <Text style={styles.checkedIcon}>
+                {item.checked ? '✔️' : '◯'}
+              </Text>
+              <Text>{item.content}</Text>
+            </View>
+          )}
+        />
+      </View>
     </SafeAreaView>
   );
 }
@@ -15,11 +69,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colors.background,
-    alignItems: 'center',
+  },
+  innerContainer: {
+    paddingHorizontal: horizontalScale(theme.spacing.spacing_20),
   },
   header: {
-    fontSize: theme.fontSize.fontSize_24,
+    fontSize: horizontalScale(theme.fontSize.fontSize_18),
     fontWeight: theme.fontWeight.bold,
-    marginTop: theme.spacing.spacing_20,
+    marginTop: verticalScale(theme.spacing.spacing_20),
+    textAlign: 'center',
   },
+  list: {
+    marginTop: verticalScale(theme.spacing.spacing_10),
+  },
+  listItem: {
+    flexDirection: 'row',
+    marginTop: verticalScale(theme.spacing.spacing_6),
+  },
+  checkedIcon: { marginRight: horizontalScale(theme.spacing.spacing_12) },
 });
---
+++ helpers/asyncStorageHelpers.ts
@@ -0,0 +1,65 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { ItemWithId } from '../App';
+
+type ResponseStatus = 'SUCCESS' | 'ERROR' | 'EMPTY';
+
+export interface StoreDataInAsyncStorageParams {
+  key: 'shoppingList';
+  value: ItemWithId[];
+}
+
+type StoreDataInAsyncStorage = ({
+  key,
+  value,
+}: StoreDataInAsyncStorageParams) => Promise<{
+  status: ResponseStatus;
+}>;
+
+export const storeDataInAsyncStorage: StoreDataInAsyncStorage = async ({
+  key,
+  value,
+}) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem(key, jsonValue);
+    return { status: 'SUCCESS' };
+  } catch (e) {
+    console.error(e);
+    return {
+      status: 'ERROR',
+    };
+  }
+};
+
+export interface GetDataFromAsyncStorageParams {
+  key: 'shoppingList';
+}
+
+type GetDataFromAsyncStorage = ({
+  key,
+}: GetDataFromAsyncStorageParams) => Promise<{
+  status: ResponseStatus;
+  data?: ItemWithId[] | null;
+}>;
+
+export const getDataFromAsyncStorage: GetDataFromAsyncStorage = async ({
+  key,
+}) => {
+  try {
+    const jsonValue = await AsyncStorage.getItem(key);
+    return jsonValue != null
+      ? { status: 'SUCCESS', data: JSON.parse(jsonValue) }
+      : { status: 'EMPTY', data: null };
+  } catch (e) {
+    console.error(e);
+    return { status: 'ERROR' };
+  }
+};
+
+export const clearAsyncStorage = async () => {
+  try {
+    await AsyncStorage.clear();
+  } catch (e) {
+    console.error(e);
+  }
+};
